31ff28ed4c81b772d5881af03d83183db250de83
Bo Feng <bfeng1216@gmail.com>
2012-03-11 21:35:28 -0500

    incompleted files

--- src/edu/iit/cs/cs553/WordCounterThread.java
+++ src/edu/iit/cs/cs553/WordCounterThread.java
@@ -1,14 +1,27 @@
-package edu.cs.iit.cs553;
+package edu.iit.cs.cs553;
 
 import java.io.File;
 
 public class WordCounterThread extends Thread {
 
+  private File inputFile;
+
   public WordCounterThread(File inputFile) {
+    this.inputFile = inputFile;
   }
 
   @Override
   public void run() {
     // this is the actual code of counting words
+    System.out.println("I'm going to process this file: "+this.inputFile.getName());
+
+    try {
+      Thread.sleep(3*1000);
+    } catch (InterruptedException e) {
+      System.err.println("I'm getting killed");
+      return;
+    }
+
+    System.out.println("I'm done");
   }
 }
