043e0403c8dbc24e0a56f34e3ef5167c45a9e898
Bo Feng <bfeng1216@gmail.com>
2012-03-13 01:54:16 -0500

    clean up the project and make it compatible with Netbeans

--- src/edu/iit/cs/cs553/WordCountMR.java
+++ src/edu/iit/cs/cs553/WordCountMR.java
@@ -1,60 +1,61 @@
 package edu.iit.cs.cs553;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.StringTokenizer;
-
 import org.apache.hadoop.fs.Path;
-import org.apache.haddop.conf.JobConf;
-import org.apache.haddop.io.*;
-import org.apache.haddop.mapred.*;
-import org.apache.haddop.util.*;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
 
 public class WordCountMR {
 
-  public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
-    
-    private final static IntWritable one = new IntWritable(1);
+    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
 
-    private Text word = new Text();
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
 
-    public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
-      String line = value.toString();
-      StringTokenizer st = new StringTokenizer(line, "?;\" \t\n\r\f");
-      while(tokenizer.hasMoreTokens()) {
-        word.set(tokenizer.nextToken());
-        output.collect(word, one);
-      }
+        @Override
+        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            String line = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(line, "?;\" \t\n\r\f");
+            while (tokenizer.hasMoreTokens()) {
+                word.set(tokenizer.nextToken());
+                output.collect(word, one);
+            }
+        }
     }
-  }
 
-  public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-    
-    public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
-      int sum = 0;
-      while (values.hasNext()) {
-        sum += values.next().get();
-      }
-      output.collect(key, new IntWritable(sum));
+    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+
+        @Override
+        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            int sum = 0;
+            while (values.hasNext()) {
+                sum += values.next().get();
+            }
+            output.collect(key, new IntWritable(sum));
+        }
     }
-  }
 
-  public static void main(String[] args) {
-    JobConf conf = new JobConf(WordCountMR.class);
-    conf.setJobName("wordcount");
+    public static void main(String[] args) throws IOException {
+        JobConf conf = new JobConf(WordCountMR.class);
+        conf.setJobName("wordcount");
 
-    conf.setOutputKeyClass(Text.class);
-    conf.setOutputValueClass(IntWritable.class);
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
 
-    conf.setMapperClass(Map.class);
-    conf.setCombinerClass(Reduce.class);
-    conf.setReducerClass(Reduce.class);
+        conf.setMapperClass(Map.class);
+        conf.setCombinerClass(Reduce.class);
+        conf.setReducerClass(Reduce.class);
 
-    conf.setInputFormat(TextInputFormat.class);
-    conf.setOutputFormat(TextOutputFormat.class);
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
 
-    FileInputFormat.setInputPaths(conf, new Path(args[0]));
-    FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+        FileInputFormat.setInputPaths(conf, new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
 
-    JobClient.runJob(conf);
-  }
+        JobClient.runJob(conf);
+    }
 }
